7cbfd85b5a97c295ea5ef0902d650a37da64ae0e
Meloakartas <thomas.couderc@viacesi.fr>
2022-08-25 11:11:16 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,38 +44,11 @@ export class AppComponent {
 
   displayModal: boolean;
 
-  displayBasic: boolean;
-
-  displayBasic2: boolean;
-
-  displayMaximizable: boolean;
-
-  displayPosition: boolean;
-
-  position: string;
-
   cities: any[];
 
   selectedCities: string[] = [];
 
   showModalDialog() {
     this.displayModal = true;
-  }
-
-  showBasicDialog() {
-    this.displayBasic = true;
-  }
-
-  showBasicDialog2() {
-    this.displayBasic2 = true;
-  }
-
-  showMaximizableDialog() {
-    this.displayMaximizable = true;
-  }
-
-  showPositionDialog(position: string) {
-    this.position = position;
-    this.displayPosition = true;
   }
 }
